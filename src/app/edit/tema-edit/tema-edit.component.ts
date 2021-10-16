cefd057c1aabba2a32fc3aca8c823f3c74b9df45
Paulopdm <paulo.mfn@gmail.com>
2021-10-16 19:48:52 -0300

    correção do botão edit

--- src/app/edit/tema-edit/tema-edit.component.ts
+++ src/app/edit/tema-edit/tema-edit.component.ts
@@ -23,7 +23,7 @@ export class TemaEditComponent implements OnInit {
 
   ngOnInit() {
 
-    if(environment.token != ''){
+    if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
     let id = this.route.snapshot.params['id']
